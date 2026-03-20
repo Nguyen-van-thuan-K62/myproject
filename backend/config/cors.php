e90e96f5afa52f4286e0d8396868b8238722afbb
Nguyen-van-thuan-K62 <nvtdhthuan@gmail.com>
2026-03-21 02:01:18 +0700

    fix cors

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -6,7 +6,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['https://myproject-five-black.vercel.app'],
 
     'allowed_headers' => ['*'],
 
